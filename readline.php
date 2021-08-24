63f0d2398dd6b1475719eefbda450f195ec2acf3
Pouya Rezvani <thesirpr@gmail.com>
2021-08-24 19:31:19 +0430

    Better logic

--- readline.php
+++ readline.php
@@ -3,8 +3,13 @@
 function read_line($prompt = '') {
 	$result = false;
 	if (php_sapi_name() === "cli") {
-		fwrite(STDOUT, $prompt);
-		$result = fgets(STDIN);
+		if(function_exists("readline")) // we use readline php extention if is availble for using
+			$result = readline($prompt);
+		else {
+			echo $prompt;
+			$result = fgets(STDIN);
+		}
+		
 	}
 	return $result;
 }
